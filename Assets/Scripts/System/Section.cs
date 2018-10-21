7507a595d43cf7a0ad5271a2238be4f49b473c34
1238qwer <1238qwer@naver.com>
2018-10-21 23:30:54 +0900

    model

--- Assets/Scripts/System/Section.cs
+++ Assets/Scripts/System/Section.cs
@@ -11,21 +11,28 @@ public class Section : ScriptableObject {
 
     private float depth;
 
+    private float testFloat;
+
     private void OnEnable()
     {
+        testFloat = 30;
         depth = 0;
     }
 
     public void Spawn()
     {
-        GameObject currentSection = Instantiate(sectionHead);
+        GameObject currentSection = Instantiate(sectionHead,new Vector3(0,0,0),Quaternion.identity);
 
         foreach (GameObject item in sectionBody)
         {
-            currentSection = Instantiate(item);
+            depth += testFloat;
+
+            currentSection = Instantiate(item, new Vector3(0, 0, 0 + depth),Quaternion.identity);          
         }
 
+        depth += testFloat;
+
         currentSection = 
-            Instantiate(sectionTail);
+            Instantiate(sectionTail,new Vector3(0, 0, 0 + depth),Quaternion.identity);
     }
 }
